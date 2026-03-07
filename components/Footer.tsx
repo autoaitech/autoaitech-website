982f6b38de152da3c5f8ace0cb5fe395a0f9d762
Sean Mulligan <seanmulligan@Seans-MacBook-Air.local>
2026-03-07 11:39:31 +0000

    Revert "feat: fix dead space, remove we/I copy, update footer nav"
    This reverts commit 54a7ea68ed1b73f0dd4da92eaeae5485f17be52a.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,8 +18,6 @@ export default function Footer() {
 
         <div className="flex gap-8">
           {[
-            { label: "Systems", href: "/#systems" },
-            { label: "How it works", href: "/#how-it-works" },
             { label: "Contact", href: "/contact" },
           ].map((l) => (
             <Link
